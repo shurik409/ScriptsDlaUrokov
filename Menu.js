3f00deac20bede6801cc4dadbe8de4833b61397f
Sasha <shurik409@gmail.com>
2013-03-02 18:53:22 +0200

    изменил качество изображения

--- Menu.js
+++ Menu.js
@@ -1,7 +1,10 @@
 var BoxRect : Rect=Rect(100,100,10,10);
-
+var BoxRect2 : Rect=Rect(200,100,65,36);
 function OnGUI(){
 if(GUI.Button(BoxRect,"Меню")){
-Application.LoadLevel(1);
+QualitySettings.currentLevel = QualityLevel.Fastest;
+}
+if(GUI.Button(BoxRect2,"Меню2")){
+QualitySettings.currentLevel = QualityLevel.Fantastic;
 }
 }
