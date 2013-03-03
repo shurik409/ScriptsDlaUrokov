b909e17f519c5a4ef9083a32f3ed62824c06e2ad
Sasha <shurik409@gmail.com>
2013-03-03 11:36:55 +0200

    полный скртпт на поднятие и кидание объекта

--- Ataka.js
+++ Ataka.js
@@ -1,9 +1,35 @@
-#pragma strict
+var grabPower = 10.0;
+var throwPower = 50;
+var hit : RaycastHit;
+var RayDistance : float = 3.0;
+private var Grab : boolean = false;
+private var Throw : boolean = false;
+var offset : Transform;
 
-function Start () {
+
+function Update(){
+if(Input.GetMouseBuuttonDown(1)){
+Physics. Raycast(transform.position, transform.forward,hit,RayDistance);
+if(hit.rigidbody){
+Grab=true;
+}
+}
+if(Input.GetMouseBuuttonDown(0)){
+if(Grab){
+Grab=false;
+Throw=true;
+}
+}
+if(Grab){
+if(hit.rigidbody){
+hit.rigidbody.velocity = (offset.position - (hit.transform.position + hit.rigidbody.centerOfMass))* grabPower;
 
 }
-
-function Update () {
-
+}
+if(Throw){
+if(hit.rigidbody){
+hit.rigidbody.velosity=transform.forward*throwPower;
+trow=false;
+}
+}
 }
